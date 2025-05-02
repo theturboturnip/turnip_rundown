e8639eea95b1a97ba304708e51fbde849007d101
Samuel Stark <samuel.winslow.stark@gmail.com>
2025-05-02 17:56:39 +0100

    Some insight tests

--- test/weather_insights_test.dart
+++ test/weather_insights_test.dart
@@ -1,5 +1,8 @@
 import 'package:test/test.dart';
+import 'package:turnip_rundown/data/units.dart';
 import 'package:turnip_rundown/data/weather/insights.dart';
+
+import 'data_units_test.dart';
 
 void main() {
   group(
@@ -66,5 +69,54 @@ void main() {
     },
   );
 
-  // TODO test actual insights
+  final genericHeatLevelMap = LevelMap(min: Heat.freezing, minValueForLevel: {
+    Heat.chilly: const Data(5, Temp.celsius),
+    Heat.mild: const Data(10, Temp.celsius),
+    Heat.warm: const Data(15, Temp.celsius),
+    Heat.hot: const Data(20, Temp.celsius),
+    Heat.boiling: const Data(25, Temp.celsius),
+  });
+
+  test(
+    "heat insight",
+    () {
+      final insight = HeatLevelInsight(
+        const DataSeries(
+          [
+            // freezing
+            1.5,
+            2.5,
+            3.5,
+            4.5,
+            // chilly
+            5.5,
+            6.5,
+            7.5,
+            8.5,
+            9.5,
+            // mild
+            10.5,
+          ],
+          Temp.celsius,
+        ),
+        genericHeatLevelMap,
+      );
+      expect(insight.min.valueAs(Temp.celsius), approxEquals(1.5));
+      expect(insight.max.valueAs(Temp.celsius), approxEquals(10.5));
+      expect(insight.levelRanges, [
+        (Heat.freezing, 0, 3),
+        (Heat.chilly, 4, 8),
+        (Heat.mild, 9, 10),
+      ]);
+      // Can't do this, list equality doesn't happen right
+      // expect(insight.nonNullLevelRanges(), [
+      //   ([Heat.freezing, Heat.chilly, Heat.mild], 0, 10),
+      // ]);
+      // so instead
+      expect(insight.nonNullLevelRanges().length, 1);
+      expect(insight.nonNullLevelRanges().first.$1, [Heat.freezing, Heat.chilly, Heat.mild]);
+      expect(insight.nonNullLevelRanges().first.$2, 0);
+      expect(insight.nonNullLevelRanges().first.$3, 10);
+    },
+  );
 }
